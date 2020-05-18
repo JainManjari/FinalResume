55073ed48ed51909099d7ef5ec30818d3697358d
manjari jain <manjarijain98@gmail.com>
2020-05-18 14:59:14 +0530

    add some more files

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,3 +9,39 @@ module.exports.home= async function(req,res)
         return;
     }
 }
+
+module.exports.calculator=async function(req,res)
+{
+    try{
+        return res.render("cal");
+    }
+    catch(err)
+    {
+        console.log("Error in loading calculator page ",err);
+        return;
+    }
+}
+
+module.exports.music=async function(req,res)
+{
+    try{
+        return res.render("music");
+    }
+    catch(err)
+    {
+        console.log("Error in loading calculator page ",err);
+        return;
+    }
+}
+
+module.exports.music2=async function(req,res)
+{
+    try{
+        return res.render("music2");
+    }
+    catch(err)
+    {
+        console.log("Error in loading calculator page ",err);
+        return;
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
 router.get("/",homeController.home);
+router.get("/calculator",homeController.calculator);
+router.get("/music-website",homeController.music);
+router.get("/music-website/singer-playlist",homeController.music2);
 
 module.exports=router;
