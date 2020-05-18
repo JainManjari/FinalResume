50487a6eecd96996ead69f3515e6649dcdc2103d
manjari jain <manjarijain98@gmail.com>
2020-05-18 14:04:57 +0530

    files loaded

---
+++ controllers/home_controller.js
@@ -0,0 +1,11 @@
+module.exports.home= async function(req,res)
+{
+    try{
+        return res.render("home");
+    }
+    catch(err)
+    {
+        console.log("Error in loading home page ",err);
+        return;
+    }
+}
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+app.use(express.urlencoded());
+
+app.use(express.static("./assets"));
+
+app.set("view engine","ejs");
+app.set("views","./views");
+
+app.use("/",require("./routes"));
+
 app.listen(port,function(err)
 {
     if(err)
