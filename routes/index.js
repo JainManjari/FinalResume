53a034ff8e990dd127e5c2a94f0707c70d917a77
manjari jain <manjarijain98@gmail.com>
2020-05-24 15:43:30 +0530

    linking routes

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
 router.get("/",homeController.home);
+router.get("/blog/article1",homeController.blog1);
 router.get("/calculator",homeController.calculator);
 router.get("/music-website",homeController.music);
 router.get("/music-website/singer-playlist",homeController.music2);
 router.get("/counter",homeController.counter);
 router.get("/ping-pong",homeController.pingpong);
 router.post("/send-message",homeController.newMessage);
-router.get("/blog/article1",homeController.blog1);
+
 
 
 module.exports=router;
